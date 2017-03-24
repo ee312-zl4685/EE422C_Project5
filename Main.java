3d03eb9d418d65b555b0fa0195cc19627066d5f5
Zhaofeng Liang <zhaofengliang@utexas.edu>
2017-03-24 14:48:52 -0500

    JavaFX initialization

--- Main.java
+++ Main.java
@@ -1,9 +1,27 @@
 package assignment5;
 
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
-	public static void main(String[] args) {
-		// launch(args);
+public class Main extends Application {
+	static GridPane grid = new GridPane();
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			grid.setGridLinesVisible(true);
+			Scene scene = new Scene(grid, 500, 500);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			// Paints the icons. Painter.paint();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
