9fee69da6e479c0b290c08ee690415f3644962ba
Zohaib Imam <kavzoh@utexas.edu>
2017-03-27 14:58:20 -0500

    Display Grid

--- Controller.java
+++ Controller.java
@@ -11,18 +11,22 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import static assignment5.Params.*;
 
 
 public class Controller {
-	
-	private static String myPackage;
-	static {
-		 myPackage = Critter.class.getPackage().toString().split(" ")[1];
-	}
-	
-	ObservableList<String> critterNameList = 
-			(ObservableList<String>) FXCollections.observableArrayList("Algae","Craig","Critter1","Critter2","Critter3","Critter4");
-	
+
+    private static String myPackage;
+    static {
+        myPackage = Critter.class.getPackage().toString().split(" ")[1];
+    }
+
+    ObservableList<String> critterNameList =
+            (ObservableList<String>) FXCollections.observableArrayList("Algae","Craig","Critter1","Critter2","Critter3","Critter4");
+
     @FXML
     private ChoiceBox<String> cbMakeCritter;
     @FXML
@@ -46,17 +50,44 @@ public class Controller {
     private TextField Speed;
     @FXML
     private TextField tfTime;
-    
+
     @FXML
     private TextArea RunStatsConsole;
 
     @FXML
+    private GridPane gridpane;
+
+    @FXML
     private void initialize(){
-    	cbMakeCritter.setValue("Algae");
-    	cbMakeCritter.setItems(critterNameList);
-    	cbRunStats.setValue("Algae");
-    	cbRunStats.setItems(critterNameList);
-    	
+        cbMakeCritter.setValue("Algae");
+        cbMakeCritter.setItems(critterNameList);
+        cbRunStats.setValue("Algae");
+        cbRunStats.setItems(critterNameList);
+        displayGrid();
+    }
+
+    private void displayGrid() {
+        int c = world_height;
+        int r = world_width;
+        int SIZE =20;
+        int GAP = 2;
+        gridpane.setVgap(GAP);
+        gridpane.setHgap(GAP);
+
+        gridpane.setGridLinesVisible(false);
+
+        //gridpane.setPrefSize(300,300);
+
+        Color[] colors =  {Color.BLACK, Color.BLUE, Color.GREEN, Color.RED};
+        for(int i =0; i< r; i++) {
+            for(int j=0; j<c; j++){
+                Rectangle rec = new Rectangle();
+                rec.setHeight(SIZE);
+                rec.setWidth(SIZE);
+                rec.setFill(colors[Critter.getRandomInt(colors.length)]);
+                gridpane.add(rec,i,j);
+            }
+        }
     }
 
     @FXML
@@ -75,9 +106,9 @@ public class Controller {
             } catch (InvalidCritterException e) {
                 e.printStackTrace();
             }
-        } 
+        }
     }
-    
+
     @FXML
     private void timeStep() throws IOException{
         String StrQuantity = tfSteps.getText();
@@ -86,7 +117,7 @@ public class Controller {
             try {
                 Quantity = Integer.parseInt(StrQuantity);
             } catch (Exception e){
-               tfSteps.setText("INTEGER!");
+                tfSteps.setText("INTEGER!");
             }
         }
 
@@ -96,30 +127,30 @@ public class Controller {
         }
     }
 
-    
+
     @FXML
     private void goRunStats() throws IOException{
-    	String type = cbRunStats.getValue();
-    	String consoleMessage = "";
-    	Class<?> class_name = null;
+        String type = cbRunStats.getValue();
+        String consoleMessage = "";
+        Class<?> class_name = null;
 
-		try {
-		    List<Critter> t = Critter.getInstances(type);
-		    class_name = Class.forName(myPackage+"." + type);
-		    Method runstats = class_name.getMethod("runStats", List.class);
-		    consoleMessage = (String) runstats.invoke(class_name, t);
+        try {
+            List<Critter> t = Critter.getInstances(type);
+            class_name = Class.forName(myPackage+"." + type);
+            Method runstats = class_name.getMethod("runStats", List.class);
+            consoleMessage = (String) runstats.invoke(class_name, t);
 
-		}
-		catch (Error e) {
-		    e.printStackTrace();
-		}
+        }
+        catch (Error e) {
+            e.printStackTrace();
+        }
 
-		catch (Exception e) {
-		    e.printStackTrace();
-		}
-		
-    	RunStatsConsole.setText(consoleMessage);
-    	
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        RunStatsConsole.setText(consoleMessage);
+
     }
 
 
